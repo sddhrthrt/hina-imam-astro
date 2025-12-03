325a88680eb3e549fbdfd64e4fa0ef36485fa913
Sid Thota <sddhrthrt@gmail.com>
2025-12-02 16:11:00 -0800

    blog?

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,15 +5,24 @@ import { defineCollection, z } from 'astro:content'
 import { glob } from 'astro/loaders'
 
 // 3. Define your collection(s)
+const posts = defineCollection({
+  loader: glob({ pattern: '**/*.mdx', base: './src/content/posts' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    draft: z.string().optional(),
+    date: z.coerce.date(),
+    tags: z.array(z.string()).default([]),
+  }),
+})
 const projects = defineCollection({
   loader: glob({ pattern: '**/*.mdx', base: './src/content/projects' }),
   schema: z.object({
     title: z.string(),
-    author: z.string(),
     description: z.string(),
     tags: z.array(z.string()).default([]),
   }),
 })
 
 // 4. Export a single `collections` object to register you collection(s)
-export const collections = { projects }
+export const collections = { posts, projects }
